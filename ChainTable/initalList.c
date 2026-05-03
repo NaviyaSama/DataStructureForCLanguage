8f7826ce9430e9a8b677df502509bb11b98e7a7b
NaviyaSama <2734360631@qq.com>
2026-05-03 12:49:21 +0800

    单链表-初始化

---
+++ ChainTable/initalList.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+typedef struct node
+{
+    ElemType data;
+    struct node *next;
+} Node;
+
+Node *initList()
+{
+    Node *head = (Node *)malloc(sizeof(Node));
+    head->data = 0;
+    head->next = NULL;
+    return head;
+}
+
+int main()
+{
+    Node *list = initList();
+    return 1;
+}
