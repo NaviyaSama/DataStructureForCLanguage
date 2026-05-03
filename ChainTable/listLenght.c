a99712655ad8fac6df4105f1eed7a3c803223d83
NaviyaSama <2734360631@qq.com>
2026-05-03 13:44:33 +0800

    单链表-获取链表长度

---
+++ ChainTable/listLenght.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+typedef struct node
+{
+    ElemType data;
+    struct node *next;
+} Node;
+
+Node *initList()
+{
+    Node *head = (Node *)malloc(sizeof(Node));
+    head->data = 0;
+    head->next = NULL;
+    return head;
+}
+
+void insertHead(Node *L, ElemType e)
+{
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->data = e;
+    p->next = L->next;
+    L->next = p;
+}
+
+void listNode(Node *L)
+{
+    Node *p = L->next;
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+// 获取尾结点
+Node *get_tail(Node *L)
+{
+    Node *p = L;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    return p;
+}
+
+// 尾插法
+Node *insertTail(Node *tail, ElemType e)
+{
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->data = e;
+    tail->next = p;
+    p->next = NULL;
+    return p;
+}
+
+// 在指定位置插入数据
+int insertNode(Node *L, int pos, ElemType e)
+{
+    Node *p = L;
+    int i = 0;
+    while (i < pos - 1)
+    {
+        p = p->next;
+        i++;
+        if (p == NULL)
+        {
+            return 0;
+        }
+    }
+
+    Node *q = (Node *)malloc(sizeof(Node));
+    q->data = e;
+    q->next = p->next;
+    p->next = q;
+    return 1;
+}
+
+int deleteNode(Node *L, int pos)
+{
+    Node *p = L;
+    int i = 0;
+    while (i < pos - 1)
+    {
+        p = p->next;
+        i++;
+        if (p == NULL)
+        {
+            return 0;
+        }
+    }
+    if (p->next == NULL)
+    {
+        printf("要删除的位置有误\n");
+        return 0;
+    }
+    Node *q = p->next;
+    p->next = q->next;
+    free(q);
+    return 1;
+}
+
+int listLenght(Node *L)
+{
+    Node *p = L;
+    int len = 0;
+    while (p != NULL)
+    {
+        p = p->next;
+        len++;
+    }
+    return len;
+}
+
+int main()
+{
+    Node *list = initList();
+    Node *tail = get_tail(list);
+    tail = insertTail(tail, 11);
+    tail = insertTail(tail, 22);
+    tail = insertTail(tail, 33);
+    tail = insertTail(tail, 44);
+    listNode(list);
+    insertNode(list, 2, 18);
+    listNode(list);
+    deleteNode(list, 3);
+    listNode(list);
+    printf("此时链表的长度为:%d\n", listLenght(list));
+    return 0;
+}
