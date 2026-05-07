0d6cd27f6057fe3546cc885bd364804b5b22ed34
NaviyaSama <2734360631@qq.com>
2026-05-07 14:25:17 +0800

    堆栈-获得栈顶元素

---
+++ Stack/getTopStack.c
@@ -0,0 +1,82 @@
+#include <Stdio.h>
+
+#define MAXSIZE 100
+typedef int ElemType;
+
+typedef struct
+{
+    ElemType data[MAXSIZE];
+    int top;
+} Stack;
+
+void initStack(Stack *s)
+{
+    s->top = -1;
+}
+
+int isEmptyStack(Stack *s)
+{
+    if (s->top == -1)
+    {
+        printf("Stack is empty.\n");
+        return 1;
+    }
+    else
+    {
+        printf("Stack is not empty. Top index: %d\n", s->top);
+        return 0;
+    }
+}
+
+int pushStack(Stack *s, ElemType e)
+{
+    if (s->top == MAXSIZE - 1)
+    {
+        printf("Stack overflow. Cannot push element %d\n", e);
+        return 0; // Push failed
+    }
+    else
+    {
+        s->top++;
+        s->data[s->top] = e;
+        return 1; // Push successful
+    }
+}
+
+ElemType popStack(Stack *s, ElemType *e)
+{
+    if (s->top == -1)
+    {
+        printf("空的\n");
+        return 0;
+    }
+    *e = s->data[s->top];
+    s->top--;
+    return 1;
+}
+
+int getTop(Stack *s, ElemType *e)
+{
+    if (s->top == -1)
+    {
+        printf("空的\n");
+        return 0;
+    }
+    *e = s->data[s->top];
+    return 1;
+}
+
+int main()
+{
+    Stack s;
+    initStack(&s);
+    pushStack(&s, 10);
+    pushStack(&s, 20);
+    pushStack(&s, 30);
+    ElemType e;
+    popStack(&s, &e);
+    printf("%d\n", e);
+    getTop(&s, &e);
+    printf("%d\n", e);
+    return 0;
+}
